d2cf6806ee0b8631321eb630eef9d0d6edde6c7a
Jason Johnson <spligak@gmail.com>
2013-05-22 18:34:16 -0500

    Add queue endpoint stubs

--- mq.go
+++ mq.go
@@ -1,19 +1,61 @@
 package main
 
 import "fmt"
+import "time"
+import "net/http"
+
+type Handler struct {
+	router *Router
+	endpoints map[string]func()
+}
+
+func (handler *Handler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	match := handler.router.Match(req.Method, req.URL.Path)
+	
+	if match != nil {
+		handler.endpoints[match.Name]()
+		return
+	}
+	
+	fmt.Println("Not found!")
+}
+
+func GetQueue() {
+	fmt.Println("Get queue")
+}
+
+func CreateQueue() {
+	fmt.Println("Create queue")
+}
+
+func DeleteQueue() {
+	fmt.Println("Delete queue")
+}
 
 func main() {
-	r := &Router{}
+	router := &Router{}
+	router.AddRoute("GetQueue", "GET", "^/(?P<queue>[a-z]+)$")
+	router.AddRoute("CreateQueue", "PUT", "^/(?P<queue>[a-z]+)$")
+	router.AddRoute("DeleteQueue", "DELETE", "^/(?P<queue>[a-z]+)$")
 
-	r.AddRoute("getQueue", "GET", "/(?P<queue>[a-z]+)")
-	r.AddRoute("createQueue", "PUT", "/(?P<queue>[a-z]+)")
-	r.AddRoute("deleteQueue", "DELETE", "/(?P<queue>[a-z]+)")
-
-	match := r.Match("DELETE", "/123")
-
-	if match == nil {
-		fmt.Println("No match found.")
+	handler := &Handler{
+		router: router,
+		endpoints: make(map[string]func()),
 	}
-
-	fmt.Println(r.Match("GET", "/myqueue"))
+	
+	// Our handler functions by name. This can easily be looked up by the name
+	// our RouteMatch contains.
+	handler.endpoints["GetQueue"] = GetQueue
+	handler.endpoints["CreateQueue"] = CreateQueue
+	handler.endpoints["DeleteQueue"] = DeleteQueue
+	
+	server := &http.Server{
+		Addr: ":8080",
+		Handler: handler,
+		ReadTimeout: 10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	
+	server.ListenAndServe()
 }
