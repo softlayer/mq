b4d73a424df74839eb6e52edee9cde117f0c1b71
Jason Johnson <spligak@gmail.com>
2013-09-17 09:10:48 -0500

    Add system-level tuning configs. Push those up during build.

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Echo a list of volume mount points.
-# echo `for i in {a..z}; do echo /mnt/vol0$i; done` | tr " " ","
-
-# Mount all volumes.
-# for i in {a..z}; do mkdir /mnt/vol0$i; mount -t glusterfs gluster-00:/vol0 /mnt/vol0$i; done
-
 set -e
 
 GO=~/go/bin/go
@@ -16,12 +10,13 @@ $GO build -o build/mq        mq.go route.go endpoint.go store.go uuid.go
 $GO build -o build/mq-mover  bin/mover.go
 $GO build -o build/mq-reaper bin/reaper.go
 
+scp etc/sysctl.conf $USER@$HOST:/etc/sysctl.conf
+scp etc/security/limits.conf $USER@$HOST:/etc/security/limits.conf
+scp etc/pam.d/common-session $USER@$HOST:/etc/pam.d/common-session
 scp etc/supervisor/conf.d/mq.conf $USER@$HOST:/etc/supervisor/conf.d/mq.conf
 
 ssh $USER@$HOST 'supervisorctl reread; supervisorctl update'
 ssh $USER@$HOST 'supervisorctl stop all'
-
-sleep 5
 
 for x in mq mq-mover mq-reaper
 do
