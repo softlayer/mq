93262e70bf592f80506ede2b7c141d4fb3754d59
Jason Johnson <spligak@gmail.com>
2013-12-27 16:18:44 -0600

    Remove site-specific build tasks

--- build.sh
+++ build.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-set -e
-
-GO=~/go/bin/go
-USER=root
-HOST=198.23.111.7
-
-$GO build -o build/mq        mq.go route.go endpoint.go store.go uuid.go
-$GO build -o build/mq-mover  bin/mover.go bin/watch.go
-$GO build -o build/mq-reaper bin/reaper.go bin/watch.go
-
-scp etc/sysctl.conf $USER@$HOST:/etc/sysctl.conf
-scp etc/security/limits.conf $USER@$HOST:/etc/security/limits.conf
-scp etc/pam.d/common-session $USER@$HOST:/etc/pam.d/common-session
-scp etc/supervisor/conf.d/mq.conf $USER@$HOST:/etc/supervisor/conf.d/mq.conf
-
-ssh $USER@$HOST 'supervisorctl reread; supervisorctl update'
-ssh $USER@$HOST 'supervisorctl stop all'
-
-for x in mq mq-mover mq-reaper
-do
-	scp build/$x $USER@$HOST:~/$x
-done
-
-ssh $USER@$HOST 'supervisorctl start all'
+go build -o build/mq        mq.go route.go endpoint.go store.go uuid.go
+go build -o build/mq-mover  bin/mover.go bin/watch.go
+go build -o build/mq-reaper bin/reaper.go bin/watch.go
